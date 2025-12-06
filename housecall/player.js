7c5bf8981900b98d01c407c7674620d0ce775fe1
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2025-12-05 23:22:21 -0600

    Add files via upload

--- housecall/player.js
+++ housecall/player.js
@@ -70,7 +70,7 @@ class playerBullet extends Phaser.Physics.Arcade.Sprite
         scene.playerBulletsHolder = scene.physics.add.group
         ({
           classType: playerBullet,
-          maxSize: 60,
+          maxSize: 360,
           runChildUpdate: true
         });
 
