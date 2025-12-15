54b12e600941fe6768a605f728a75b54a966ec2f
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2025-12-15 02:03:36 -0600

    Add files via upload

--- js/launch.js
+++ js/launch.js
@@ -7,8 +7,10 @@ function createLaunchCommands()
 		{ name: "fp2rbpr", url: "./fp/index.html", display: false},
 		{ name: "housecall", url: "./housecall/index.html", display: true},
 		{ name: "jumpgame", url: "./jumpgame/index.html", display: false},
-		{ name: "vp1", url: "https://ragusta.com/index.html", display: false}
-	]
+		{ name: "vp1", url: "https://ragusta.com/index.html", display: false},
+		{ name: "genericinvitation", url: "./genericinvitation/invitation.html", display: false},
+		{ name: "genericad", url: "./genericad/digitalSignStart.html", display: false}
+	];
 
 	launchTargets = JSON.parse(JSON.stringify(defaultLaunchTargets));
 
