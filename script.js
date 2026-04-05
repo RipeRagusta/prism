54d1a9d99fbdf7789f79d9ba8888954440b62d44
RipeRagusta <132228468+RipeRagusta@users.noreply.github.com>
2026-04-05 01:30:37 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 var logoRotation;
+var logo;
 
 function initalize()
 {
 	logoRotation = 0;
+	logo = document.getElementById("logo");
+	logo.parentElement.style.perspective = "1000px";
+	flipLogo();
+	updateYear();
 }
 
 function flipLogo()
 {
-	var logo = document.getElementById("logo");
 	logoRotation += 180;
-	logo.style.setProperty("--rotation", `${logoRotation}deg`);
+	logo.style.transform = "rotateY(" + logoRotation + "deg)";
+}
+
+function updateYear()
+{
+	var footerDefaultString = "2025 - ";
+	var newFooterString = footerDefaultString + new Date().getFullYear();
+	document.getElementById("year").textContent = newFooterString + " ";
 }
